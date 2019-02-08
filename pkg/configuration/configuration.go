3a2508fc943415b3443e4e8b79e6874dd375ac3e
Mike Johanson <michael.johanson@intel.com>
2019-02-08 11:13:41 -0700

    removing json configuration

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -16,13 +16,6 @@
 
 package configuration
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-)
-
 // Configuration struct which contains
 // an array of bindings
 type Configuration struct {
@@ -36,24 +29,4 @@ type Binding struct {
 	Direction string `json:"direction"`
 	Name      string `json:"name"`
 	Topic     string `json:"topic"`
-}
-
-// LoadConfiguration loads the configuration from file
-func LoadConfiguration() Configuration {
-	// Open our jsonFile
-	jsonFile, err := os.Open("config.json")
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened config.json")
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var result Configuration
-	json.Unmarshal([]byte(byteValue), &result)
-
-	return result
 }
