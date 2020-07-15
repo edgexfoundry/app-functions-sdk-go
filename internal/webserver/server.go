edgexfoundry/device-sdk-go

commit e8b256540df231ab09ea6c5b4df979531e84fc49

feat: configurable ListenAndServe address, fixes #405 (#406)
Signed-off-by: charles-knox-intel <44684517+charles-knox-intel@users.noreply.github.com>

internal/webserver/server.go
@@ -341,13 +341,20 @@ func (webserver *WebServer) StartWebServer(errChannel chan error) {
 // Helper function to handle HTTPs or HTTP connection based on the configured protocol
 func listenAndServe(webserver *WebServer, serviceTimeout time.Duration, errChannel chan error) {
 
-	p := fmt.Sprintf(":%d", webserver.Config.Service.Port)
+	// this allows env overrides to explicitly set the value used
+	// for ListenAndServe, as needed for different deployments
+	addr := fmt.Sprintf("%v:%d", webserver.Config.Service.ServerBindAddr, webserver.Config.Service.Port)
+	// for backwards compatibility, the Host value is the default value if
+	// the ServerBindAddr value is not specified
+	if webserver.Config.Service.ServerBindAddr == "" {
+		addr = fmt.Sprintf("%v:%d", webserver.Config.Service.Host, webserver.Config.Service.Port)
+	}
 
 	if webserver.Config.Service.Protocol == "https" {
-		webserver.LoggingClient.Info(fmt.Sprintf("Starting HTTPS Web Server on port :%d", webserver.Config.Service.Port))
-		errChannel <- http.ListenAndServeTLS(p, webserver.Config.Service.HTTPSCert, webserver.Config.Service.HTTPSKey, http.TimeoutHandler(webserver.router, serviceTimeout, "Request timed out"))
+		webserver.LoggingClient.Info(fmt.Sprintf("Starting HTTPS Web Server on address %v", addr))
+		errChannel <- http.ListenAndServeTLS(addr, webserver.Config.Service.HTTPSCert, webserver.Config.Service.HTTPSKey, http.TimeoutHandler(webserver.router, serviceTimeout, "Request timed out"))
 	} else {
-		webserver.LoggingClient.Info(fmt.Sprintf("Starting HTTP Web Server on port :%d", webserver.Config.Service.Port))
-		errChannel <- http.ListenAndServe(p, http.TimeoutHandler(webserver.router, serviceTimeout, "Request timed out"))
+		webserver.LoggingClient.Info(fmt.Sprintf("Starting HTTP Web Server on address %v", addr))
+		errChannel <- http.ListenAndServe(addr, http.TimeoutHandler(webserver.router, serviceTimeout, "Request timed out"))
 	}
 }

internal/common/config.go
@@ -72,16 +72,17 @@ type ConfigurationStruct struct {
 
 // ServiceInfo is used to hold and configure various settings related to the hosting of this service
 type ServiceInfo struct {
-	BootTimeout   string
-	CheckInterval string
-	Host          string
-	HTTPSCert     string
-	HTTPSKey      string
-	Port          int
-	Protocol      string
-	StartupMsg    string
-	ReadMaxLimit  int
-	Timeout       string
+	BootTimeout    string
+	CheckInterval  string
+	Host           string
+	HTTPSCert      string
+	HTTPSKey       string
+	ServerBindAddr string
+	Port           int
+	Protocol       string
+	StartupMsg     string
+	ReadMaxLimit   int
+	Timeout        string
 }
 
 // BindingInfo contains Metadata associated with each binding

edgexfoundry/app-functions-sdk-go

commit ec5ce9ac6953794dc1f2fa755244dfee1e0b4ed7

fix: backwards compatibility broken by #406, fixes #408
Signed-off-by: charles-knox-intel <44684517+charles-knox-intel@users.noreply.github.com>

internal/webserver/server.go
@@ -344,11 +344,6 @@ func listenAndServe(webserver *WebServer, serviceTimeout time.Duration, errChann
 	// this allows env overrides to explicitly set the value used
 	// for ListenAndServe, as needed for different deployments
 	addr := fmt.Sprintf("%v:%d", webserver.Config.Service.ServerBindAddr, webserver.Config.Service.Port)
-	// for backwards compatibility, the Host value is the default value if
-	// the ServerBindAddr value is not specified
-	if webserver.Config.Service.ServerBindAddr == "" {
-		addr = fmt.Sprintf("%v:%d", webserver.Config.Service.Host, webserver.Config.Service.Port)
-	}
 
 	if webserver.Config.Service.Protocol == "https" {
 		webserver.LoggingClient.Info(fmt.Sprintf("Starting HTTPS Web Server on address %v", addr))