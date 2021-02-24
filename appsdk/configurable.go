23c0d554464aae258c7ba4067fdec97afafbba4d
lenny <leonard.goodell@intel.com>
2021-02-24 13:29:22 -0700

    refactor: Rework secrets for HTTP Export so value in InsecureSecrets can be overridden
    closes #521
    
    BREAKING CHANGE: Parameters have changed for HTTP Post/Put with SecretHeader
    
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- appsdk/configurable.go
+++ appsdk/configurable.go
@@ -36,7 +36,6 @@ const (
 	Url                 = "url"
 	MimeType            = "mimetype"
 	PersistOnError      = "persistonerror"
-	Cert                = "cert"
 	SkipVerify          = "skipverify"
 	Qos                 = "qos"
 	Retain              = "retain"
@@ -46,7 +45,7 @@ const (
 	Rule                = "rule"
 	BatchThreshold      = "batchthreshold"
 	TimeInterval        = "timeinterval"
-	SecretHeaderName    = "secretheadername"
+	HeaderName          = "headername"
 	SecretPath          = "secretpath"
 	SecretName          = "secretname"
 	BrokerAddress       = "brokeraddress"
@@ -173,7 +172,7 @@ func (dynamic AppFunctionsSDKConfigurable) FilterByResourceName(parameters map[s
 
 // TransformToXML transforms an EdgeX event to XML.
 // It will return an error and stop the pipeline if a non-edgex
-// event is received or if no data is recieved.
+// event is received or if no data is received.
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) TransformToXML() appcontext.AppFunction {
 	transform := transforms.Conversion{}
@@ -182,7 +181,7 @@ func (dynamic AppFunctionsSDKConfigurable) TransformToXML() appcontext.AppFuncti
 
 // TransformToJSON transforms an EdgeX event to JSON.
 // It will return an error and stop the pipeline if a non-edgex
-// event is received or if no data is recieved.
+// event is received or if no data is received.
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) TransformToJSON() appcontext.AppFunction {
 	transform := transforms.Conversion{}
@@ -213,14 +212,14 @@ func (dynamic AppFunctionsSDKConfigurable) PushToCore(parameters map[string]stri
 	return transform.PushToCoreData
 }
 
-// CompressWithGZIP compresses data received as either a string,[]byte, or json.Marshaler using gzip algorithm and returns a base64 encoded string as a []byte.
+// CompressWithGZIP compresses data received as either a string,[]byte, or json.Marshaller using gzip algorithm and returns a base64 encoded string as a []byte.
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) CompressWithGZIP() appcontext.AppFunction {
 	transform := transforms.Compression{}
 	return transform.CompressWithGZIP
 }
 
-// CompressWithZLIB compresses data received as either a string,[]byte, or json.Marshaler using zlib algorithm and returns a base64 encoded string as a []byte.
+// CompressWithZLIB compresses data received as either a string,[]byte, or json.Marshaller using zlib algorithm and returns a base64 encoded string as a []byte.
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) CompressWithZLIB() appcontext.AppFunction {
 	transform := transforms.Compression{}
@@ -256,14 +255,14 @@ func (dynamic AppFunctionsSDKConfigurable) EncryptWithAES(parameters map[string]
 		return nil
 	}
 
-	transforms := transforms.Encryption{
+	transform := transforms.Encryption{
 		EncryptionKey:        encryptionKey,
 		InitializationVector: initVector,
 		SecretPath:           secretPath,
 		SecretName:           secretName,
 	}
 
-	return transforms.EncryptWithAES
+	return transform.EncryptWithAES
 }
 
 // HTTPPost will send data from the previous function to the specified Endpoint via http POST. If no previous function exists,
@@ -271,42 +270,20 @@ func (dynamic AppFunctionsSDKConfigurable) EncryptWithAES(parameters map[string]
 // method will default to application/json.
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) HTTPPost(parameters map[string]string) appcontext.AppFunction {
-	var err error
-
-	url, ok := parameters[Url]
-	if !ok {
-		dynamic.Sdk.LoggingClient.Error("HTTPPost Could not find " + Url)
-		return nil
-	}
-	mimeType, ok := parameters[MimeType]
-	if !ok {
-		dynamic.Sdk.LoggingClient.Error("HTTPPost Could not find " + MimeType)
+	url, mimeType, persistOnError, headerName, secretPath, secretName, err := dynamic.processPostPutParameters(parameters)
+	if err != nil {
+		dynamic.Sdk.LoggingClient.Error(err.Error())
 		return nil
 	}
 
-	// PersistOnError is optional and is false by default.
-	persistOnError := false
-	value, ok := parameters[PersistOnError]
-	if ok {
-		persistOnError, err = strconv.ParseBool(value)
-		if err != nil {
-			dynamic.Sdk.LoggingClient.Error(fmt.Sprintf("HTTPPost Could not parse '%s' to a bool for '%s' parameter", value, PersistOnError), "error", err)
-			return nil
-		}
-	}
-
-	url = strings.TrimSpace(url)
-	mimeType = strings.TrimSpace(mimeType)
-
-	secretHeaderName := parameters[SecretHeaderName]
-	secretPath := parameters[SecretPath]
 	var transform transforms.HTTPSender
-	if secretHeaderName != "" && secretPath != "" {
-		transform = transforms.NewHTTPSenderWithSecretHeader(url, mimeType, persistOnError, secretHeaderName, secretPath)
+	if len(secretPath) != 0 {
+		transform = transforms.NewHTTPSenderWithSecretHeader(url, mimeType, persistOnError, headerName, secretPath, secretName)
 	} else {
 		transform = transforms.NewHTTPSender(url, mimeType, persistOnError)
 	}
-	dynamic.Sdk.LoggingClient.Debug("HTTPPost Parameters", Url, transform.URL, MimeType, transform.MimeType)
+
+	dynamic.Sdk.LoggingClient.Debugf("HTTPPost Parameters: %v", parameters)
 	return transform.HTTPPost
 }
 
@@ -331,41 +308,19 @@ func (dynamic AppFunctionsSDKConfigurable) HTTPPostXML(parameters map[string]str
 // method will default to application/json.
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) HTTPPut(parameters map[string]string) appcontext.AppFunction {
-	var err error
-
-	url, ok := parameters[Url]
-	if !ok {
-		dynamic.Sdk.LoggingClient.Error("HTTPPut Could not find " + Url)
-		return nil
-	}
-	mimeType, ok := parameters[MimeType]
-	if !ok {
-		dynamic.Sdk.LoggingClient.Error("HTTPPut Could not find " + MimeType)
+	url, mimeType, persistOnError, headerName, secretPath, secretName, err := dynamic.processPostPutParameters(parameters)
+	if err != nil {
+		dynamic.Sdk.LoggingClient.Error(err.Error())
 		return nil
 	}
 
-	// PersistOnError is optional and is false by default.
-	persistOnError := false
-	value, ok := parameters[PersistOnError]
-	if ok {
-		persistOnError, err = strconv.ParseBool(value)
-		if err != nil {
-			dynamic.Sdk.LoggingClient.Error(fmt.Sprintf("HTTPPut Could not parse '%s' to a bool for '%s' parameter", value, PersistOnError), "error", err)
-			return nil
-		}
-	}
-
-	url = strings.TrimSpace(url)
-	mimeType = strings.TrimSpace(mimeType)
-
-	secretHeaderName := parameters[SecretHeaderName]
-	secretPath := parameters[SecretPath]
 	var transform transforms.HTTPSender
-	if secretHeaderName != "" && secretPath != "" {
-		transform = transforms.NewHTTPSenderWithSecretHeader(url, mimeType, persistOnError, secretHeaderName, secretPath)
+	if len(secretPath) != 0 {
+		transform = transforms.NewHTTPSenderWithSecretHeader(url, mimeType, persistOnError, headerName, secretPath, secretName)
 	} else {
 		transform = transforms.NewHTTPSender(url, mimeType, persistOnError)
 	}
+
 	dynamic.Sdk.LoggingClient.Debug("HTTPPut Parameters", Url, transform.URL, MimeType, transform.MimeType)
 	return transform.HTTPPut
 }
@@ -387,7 +342,7 @@ func (dynamic AppFunctionsSDKConfigurable) HTTPPutXML(parameters map[string]stri
 }
 
 // SetOutputData sets the output data to that passed in from the previous function.
-// It will return an error and stop the pipeline if data passed in is not of type []byte, string or json.Mashaler
+// It will return an error and stop the pipeline if data passed in is not of type []byte, string or json.Marshaller
 // This function is a configuration function and returns a function pointer.
 func (dynamic AppFunctionsSDKConfigurable) SetOutputData(parameters map[string]string) appcontext.AppFunction {
 	transform := transforms.OutputData{}
@@ -597,4 +552,52 @@ func (dynamic AppFunctionsSDKConfigurable) AddTags(parameters map[string]string)
 	dynamic.Sdk.LoggingClient.Debug("Add Tags", Tags, fmt.Sprintf("%v", tags))
 
 	return transform.AddTags
+}
+
+func (dynamic AppFunctionsSDKConfigurable) processPostPutParameters(
+	parameters map[string]string) (string, string, bool, string, string, string, error) {
+	url, ok := parameters[Url]
+	if !ok {
+		return "", "", false, "", "", "", fmt.Errorf("HTTPPut Could not find %s", Url)
+	}
+	mimeType, ok := parameters[MimeType]
+	if !ok {
+		return "", "", false, "", "", "", fmt.Errorf("HTTPPut Could not find %s", MimeType)
+	}
+
+	// PersistOnError is optional and is false by default.
+	persistOnError := false
+	value, ok := parameters[PersistOnError]
+	if ok {
+		var err error
+		persistOnError, err = strconv.ParseBool(value)
+		if err != nil {
+			return "", "", false, "", "", "",
+				fmt.Errorf("HTTPPut Could not parse '%s' to a bool for '%s' parameter: %s",
+					value,
+					PersistOnError,
+					err.Error())
+		}
+	}
+
+	url = strings.TrimSpace(url)
+	mimeType = strings.TrimSpace(mimeType)
+	headerName := strings.TrimSpace(parameters[HeaderName])
+	secretPath := strings.TrimSpace(parameters[SecretPath])
+	secretName := strings.TrimSpace(parameters[SecretName])
+
+	if len(headerName) == 0 && len(secretPath) != 0 && len(secretName) != 0 {
+		return "", "", false, "", "", "",
+			fmt.Errorf("HTTPPost missing %s since %s & %s are specified", HeaderName, SecretPath, SecretName)
+	}
+	if len(secretPath) == 0 && len(headerName) != 0 && len(secretName) != 0 {
+		return "", "", false, "", "", "",
+			fmt.Errorf("HTTPPost missing %s since %s & %s are specified", SecretPath, HeaderName, SecretName)
+	}
+	if len(secretName) == 0 && len(secretPath) != 0 && len(headerName) != 0 {
+		return "", "", false, "", "", "",
+			fmt.Errorf("HTTPPost missing %s since %s & %s are specified", SecretName, SecretPath, HeaderName)
+	}
+
+	return url, mimeType, persistOnError, headerName, secretPath, secretName, nil
 }
