9fe931d24613006c7a9ce3bf342484b087d4730c
lenny <leonard.goodell@intel.com>
2021-06-10 13:28:06 -0700

    refactor: Update code for latest core contracts V1 removal
    Updated to all latest go-mods
    Refactored code for new import paths and removed v1 code
    Refactored Version check to use new V2 Common Client.
    
    closes #885
    
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- pkg/transforms/compression_test.go
+++ pkg/transforms/compression_test.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/common"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
