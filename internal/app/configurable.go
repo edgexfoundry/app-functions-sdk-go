19e77ea94902d50ea7c3e083db6ba96d872b7028
Marc-Philippe Fuller <marc-philippe.fuller@intel.com>
2023-01-04 12:05:18 -0800

    refactor(sdk): remove deprecated EncryptWithAES code (#1235)
    * refactor(sdk): remove deprecated EncryptWithAES code
    
    BRAKING CHANGE: remove deprecated EncryptionWithAES code, use AESProtection
    
    CLoses: #1227
    
    Signed-off-by: Marc-Philippe Fuller <marc-philippe.fuller@intel.com>

--- internal/app/configurable.go
+++ internal/app/configurable.go
@@ -73,7 +73,6 @@ const (
 	Algorithm           = "algorithm"
 	CompressGZIP        = "gzip"
 	CompressZLIB        = "zlib"
-	EncryptAES          = "aes"
 	EncryptAES256       = "aes256"
 	Mode                = "mode"
 	BatchByCount        = "bycount"
@@ -378,21 +377,6 @@ func (app *Configurable) Encrypt(parameters map[string]string) interfaces.AppFun
 	}
 
 	switch strings.ToLower(algorithm) {
-	case EncryptAES:
-		initVector, ok := parameters[InitVector]
-		if !ok {
-			app.lc.Error("Could not find " + InitVector)
-			return nil
-		}
-
-		//nolint: staticcheck
-		transform := transforms.Encryption{
-			EncryptionKey:        encryptionKey,
-			InitializationVector: initVector,
-			SecretPath:           secretPath,
-			SecretName:           secretName,
-		}
-		return transform.EncryptWithAES
 	case EncryptAES256:
 		if len(secretPath) > 0 && len(secretName) > 0 {
 			protector := transforms.AESProtection{
@@ -405,9 +389,8 @@ func (app *Configurable) Encrypt(parameters map[string]string) interfaces.AppFun
 		return nil
 	default:
 		app.lc.Errorf(
-			"Invalid encryption algorithm '%s'. Must be one of '%s', '%s",
+			"Invalid encryption algorithm '%s'. Must be '%s",
 			algorithm,
-			EncryptAES,
 			EncryptAES256)
 		return nil
 	}
