1be498e1ad676816747fa5bbbe5c025d98b87dd2
ejlee-intel <elizabeth.j.lee@intel.com>
2023-02-14 12:07:30 -0700

    feat!: updates for common config (#1304)
    * feat!: updates for common config
    
    BREAKING CHANGE: configuration file changed to remove common config settings
    
    Signed-off-by: Elizabeth J Lee <elizabeth.j.lee@intel.com>

--- internal/common/config.go
+++ internal/common/config.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2020 Intel Corporation
+// Copyright (c) 2023 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -161,10 +161,6 @@ type Credentials struct {
 func (c *ConfigurationStruct) UpdateFromRaw(rawConfig interface{}) bool {
 	configuration, ok := rawConfig.(*ConfigurationStruct)
 	if ok {
-		// Check that information was successfully read from Registry
-		if configuration.Service.Port == 0 {
-			return false
-		}
 		*c = *configuration
 	}
 	return ok
