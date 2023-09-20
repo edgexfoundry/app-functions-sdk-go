9cd28d4d4a5bc8db9656689291c11025f19df96c
Leonard Goodell <leonard.goodell@intel.com>
2023-09-20 14:42:20 -0700

    docs: Update copyrights
    Signed-off-by: Leonard Goodell <leonard.goodell@intel.com>

--- internal/appfunction/context.go
+++ internal/appfunction/context.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2021 Intel Corporation
+// Copyright (c) 2023 Intel Corporation
 // Copyright (c) 2021 One Track Consulting
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
